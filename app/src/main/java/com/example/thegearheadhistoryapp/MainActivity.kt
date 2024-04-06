60134fe124f099c38b6c2a972755a43b0a4cfdc0
Trxy Jxy <titusphetlhe@gmail.com>
2024-04-06 11:48:32 +0200

    The Gearhead Histrory App

--- app/src/main/java/com/example/thegearheadhistoryapp/MainActivity.kt
+++ app/src/main/java/com/example/thegearheadhistoryapp/MainActivity.kt
@@ -1,85 +1,33 @@
 package com.example.thegearheadhistoryapp
 
-import android.util.Log.*
-import androidx.activity.enableEdgeToEdge
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
-import android.annotation.SuppressLint
-import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.appcompat.app.AppCompatActivity
 
-data class Driver(
-    val name: String,
-    val age: Int,
-    val description: String,
-    val reference: String
-        )
+class MainActivity : AppCompatActivity(), Parcelable {
 
-            class MainActivity : AppCompatActivity() {
+                override fun describeContents(): Int {
+                    TODO("Not yet implemented")
+                }
 
-                private val drivers = listOf(
-                    Driver(
-                        "Ayrton Senna",
-                        34,
-                        "The Brazilian driver is known for him being 3-time Formula One World Champion with 65 pole positions and winning 41 Grand Prix, with the former being the record until 2006.",
-                        "Ref: Ayrton Senna | Formula 1®"
-                    ),
-                    Driver(
-                        "James Hunt",
-                        45,
-                        "The British driver is known for him being 1-time Formula One World Champion with 14 pole positions and winning 10 Grand Prix, with 8 fastest laps, as well as being involved in one of the greatest Formula One rivalry with Niki Lauda.",
-                        "Ref: James Hunt| Formula 1®"
-                    ),
-                    // Add other drivers here
-                )
+                override fun writeToParcel(dest: Parcel, flags: Int) {
+                    TODO("Not yet implemented")
+                }
 
-                @SuppressLint("MissingInflatedId")
-                override fun onCreate(savedInstanceState: Bundle?) {
-                    super.onCreate(savedInstanceState)
-                    setContentView(R.layout.activity_main)
-
-                    val compareButton = findViewById<Button>(R.id.compareButton)
-                    val clearButton = findViewById<Button>(R.id.clearButton)
-                    val ageEditText = findViewById<EditText>(R.id.ageEditText)
-                    val resultTextView = findViewById<resultTextView>(/* id = */ R.id.resultTextView)
-
-                    compareButton.setOnClickListener {
-                        val userInput = ageEditText.text.toString()
-                        Log.v("MainActivity", "User input: $userInput")
-
-                        if (userInput.isEmpty()) {
-                            resultTextView.text = "Please enter a valid age – it must be a whole number without any decimals or text."
-                            return@setOnClickListener
-                        }
-
-                        val userAge = userInput.toIntOrNull()
-
-                        if (userAge == null) {
-                            resultTextView.text = "Please enter a valid age – it must be a whole number without any decimals or text."
-                        } else {
-                            var foundDriver = false
-                            for (driver in drivers) {
-                                if (driver.age == userAge) {
-                                    resultTextView.text = driver.description
-                                    foundDriver = true
-                                    break
-                                }
-                            }
-                            if (!foundDriver) {
-                                resultTextView.text = "No driver found with the entered age."
-                            }
-                        }
+                companion object CREATOR : Parcelable.Creator<MainActivity> {
+                    override fun createFromParcel(parcel: Parcel): MainActivity {
+                        return MainActivity()
                     }
 
-                    clearButton.setOnClickListener {
-                        ageEditText.text.clear()
-                        resultTextView.text = ""
+                    override fun newArray(size: Int): Array<MainActivity?> {
+                        return arrayOfNulls(size)
                     }
                 }
+
+
+            }
+
+
+
+
 
